9810957bad1ea26821cad756435915b2393249a7
Sanket Patel <sanpatel323@gmail.com>
2024-07-03 10:57:38 +0530

    Second Commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://pro-manage-gamma.vercel.app/api",
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://pro-manage-gamma.vercel.app/api",
+  //       changeOrigin: true,
+  //       secure: true,
+  //     }
+  //   }
+  // }
 })
